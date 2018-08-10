0d3bf25b25f75df8930a56549bae57c4f7d89b54
pjx <pujx@jingbo360.com>
2018-08-10 10:06:52 +0800

    Initial Commit

---
+++ JXPagingView/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  JXPagingView
+//
+//  Created by jiaxin on 2018/8/10.
+//  Copyright © 2018年 jiaxin. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
