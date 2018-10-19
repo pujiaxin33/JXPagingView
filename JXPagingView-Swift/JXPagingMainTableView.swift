a3ebafcdf4a52fa20078981ccc141efe134909f7
pjx <pujx@jingbo360.com>
2018-10-19 11:06:22 +0800

    添加JXPagingMainTableViewGestureDelegate

--- JXPagingView-Swift/JXPagingMainTableView.swift
+++ JXPagingView-Swift/JXPagingMainTableView.swift
@@ -8,9 +8,19 @@
 
 import UIKit
 
+@objc public protocol JXPagingMainTableViewGestureDelegate {
+    //如果headerView（或其他地方）有水平滚动的scrollView，当其正在左右滑动的时候，就不能让列表上下滑动，所以有此代理方法进行对应处理
+    func mainTableViewGestureRecognizer(_ gestureRecognizer: UIGestureRecognizer, shouldRecognizeSimultaneouslyWith otherGestureRecognizer: UIGestureRecognizer) -> Bool
+}
+
 open class JXPagingMainTableView: UITableView, UIGestureRecognizerDelegate {
+    open weak var gestureDelegate: JXPagingMainTableViewGestureDelegate?
 
     public func gestureRecognizer(_ gestureRecognizer: UIGestureRecognizer, shouldRecognizeSimultaneouslyWith otherGestureRecognizer: UIGestureRecognizer) -> Bool {
-        return gestureRecognizer.isKind(of: UIPanGestureRecognizer.classForCoder()) && otherGestureRecognizer.isKind(of: UIPanGestureRecognizer.classForCoder())
+        if gestureDelegate != nil {
+            return gestureDelegate!.mainTableViewGestureRecognizer(gestureRecognizer, shouldRecognizeSimultaneouslyWith:otherGestureRecognizer)
+        }else {
+            return gestureRecognizer.isKind(of: UIPanGestureRecognizer.classForCoder()) && otherGestureRecognizer.isKind(of: UIPanGestureRecognizer.classForCoder())
+        }
     }
 }
