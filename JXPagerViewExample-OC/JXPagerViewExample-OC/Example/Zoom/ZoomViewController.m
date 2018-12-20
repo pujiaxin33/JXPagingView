e3dd05c0ba15053a59e986303746c2a10035edb4
pujiaxin33 <317437084@qq.com>
2018-12-20 10:57:06 +0800

    添加滚动列表示例

--- JXPagerViewExample-OC/JXPagerViewExample-OC/Example/Zoom/ZoomViewController.m
+++ JXPagerViewExample-OC/JXPagerViewExample-OC/Example/Zoom/ZoomViewController.m
@@ -10,6 +10,19 @@
 
 @implementation ZoomViewController
 
+- (void)viewDidLoad {
+    [super viewDidLoad];
+
+    self.navigationItem.rightBarButtonItem = [[UIBarButtonItem alloc] initWithTitle:@"滚动列表" style:UIBarButtonItemStylePlain target:self action:@selector(scrollToTargetItem)];
+}
+
+- (void)scrollToTargetItem {
+    //1、必须让self.pagerView.mainTableView滚动到headerView刚好消失不见，才能触发列表的滚动
+    [self.pagerView.mainTableView setContentOffset:CGPointMake(0, JXTableHeaderViewHeight) animated:NO];
+    UITableView *listView = (UITableView *)self.listViewArray.firstObject.listScrollView;
+    [listView scrollToRowAtIndexPath:[NSIndexPath indexPathForRow:10 inSection:0] atScrollPosition:UITableViewScrollPositionTop animated:YES];
+}
+
 - (void)mainTableViewDidScroll:(UIScrollView *)scrollView {
     [self.userHeaderView scrollViewDidScroll:scrollView.contentOffset.y];
 }
