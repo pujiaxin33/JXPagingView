88058195cd1728c53d6dd0b77848661939cc2568
pujiaxin33 <317437084@qq.com>
2019-04-23 11:27:45 +0800

    修复旋转或者其他时机触发的循环设置contentoffset crash

--- JXPagerView/JXPagerView.m
+++ JXPagerView/JXPagerView.m
@@ -204,8 +204,6 @@
             //因为设置了contentInset.top，所以顶部会有对应高度的空白区间，所以需要设置负数抵消掉
             if (scrollView.contentOffset.y >= 0) {
                 [self adjustMainScrollViewToTargetContentInsetIfNeeded:UIEdgeInsetsMake(-scrollView.contentOffset.y, 0, 0, 0)];
-            }else {
-                [self adjustMainScrollViewToTargetContentInsetIfNeeded:UIEdgeInsetsZero];
             }
         }else if (scrollView.contentOffset.y > self.pinSectionHeaderVerticalOffset){
             //固定的位置就是contentInset.top
@@ -223,6 +221,9 @@
 - (void)scrollViewDidEndDecelerating:(UIScrollView *)scrollView {
     if (self.isListHorizontalScrollEnabled) {
         self.listContainerView.collectionView.scrollEnabled = YES;
+    }
+    if (self.mainTableView.contentInset.top != 0) {
+        [self adjustMainScrollViewToTargetContentInsetIfNeeded:UIEdgeInsetsZero];
     }
 }
 
--- JXPagingView-Swift/JXPagingView.swift
+++ JXPagingView-Swift/JXPagingView.swift
@@ -355,8 +355,6 @@ extension JXPagingView: UITableViewDataSource, UITableViewDelegate {
                 //因为设置了contentInset.top，所以顶部会有对应高度的空白区间，所以需要设置负数抵消掉
                 if scrollView.contentOffset.y >= 0 {
                     adjustMainScrollViewToTargetContentInsetIfNeeded(inset: UIEdgeInsets(top: -scrollView.contentOffset.y, left: 0, bottom: 0, right: 0))
-                }else {
-                    adjustMainScrollViewToTargetContentInsetIfNeeded(inset: UIEdgeInsets.zero)
                 }
             }else if scrollView.contentOffset.y > pinSectionHeaderVerticalOffset {
                 //固定的位置就是contentInset.top
@@ -372,6 +370,9 @@ extension JXPagingView: UITableViewDataSource, UITableViewDelegate {
     public func scrollViewDidEndDecelerating(_ scrollView: UIScrollView) {
         if isListHorizontalScrollEnabled {
             self.listContainerView.collectionView.isScrollEnabled = true
+        }
+        if mainTableView.contentInset.top != 0 {
+            adjustMainScrollViewToTargetContentInsetIfNeeded(inset: UIEdgeInsets.zero)
         }
     }
 
