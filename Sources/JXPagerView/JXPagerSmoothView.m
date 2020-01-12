68025275121bd9d47ff374dd9646b67bbcf393c7
suqiang <qiang.su@linkkids.cn>
2020-01-12 12:33:01 +0800

    位置移动，否则影响刷新

--- Sources/JXPagerView/JXPagerSmoothView.m
+++ Sources/JXPagerView/JXPagerSmoothView.m
@@ -228,9 +228,10 @@ static NSString *JXPagerSmoothViewCollectionViewCellIdentifier = @"cell";
             CGFloat minContentSizeHeight = self.bounds.size.height - self.heightForPinHeader;
             if (minContentSizeHeight > scrollView.contentSize.height) {
                 scrollView.contentSize = CGSizeMake(scrollView.contentSize.width, minContentSizeHeight);
+                scrollView.contentInset = UIEdgeInsetsMake(self.heightForPagerHeaderContainerView, 0, 0, 0);
+                scrollView.contentOffset = CGPointMake(0, -scrollView.contentInset.top + MIN(-self.currentPagerHeaderContainerViewY, self.heightForPagerHeader));
             }
-            scrollView.contentInset = UIEdgeInsetsMake(self.heightForPagerHeaderContainerView, 0, 0, 0);
-            scrollView.contentOffset = CGPointMake(0, -scrollView.contentInset.top + MIN(-self.currentPagerHeaderContainerViewY, self.heightForPagerHeader));
+            
         }
     }else {
         [super observeValueForKeyPath:keyPath ofObject:object change:change context:context];
