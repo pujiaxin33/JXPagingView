ba25ba24062de5261064eec5c88fc6d26ec05355
pujiaxin33 <317437084@qq.com>
2020-01-21 16:14:29 +0800

    优化swift mainTableView scrollViewDidScroll的处理

--- Sources/JXPagingView/JXPagingView.swift
+++ Sources/JXPagingView/JXPagingView.swift
@@ -265,15 +265,9 @@ extension JXPagingView: UITableViewDataSource, UITableViewDelegate {
     public func scrollViewDidScroll(_ scrollView: UIScrollView) {
         if pinSectionHeaderVerticalOffset != 0 {
             if scrollView.contentOffset.y < CGFloat(pinSectionHeaderVerticalOffset) {
-                if scrollView.contentOffset.y >= mainTableViewMaxContentOffsetY() {
-                    //处理mainTableViewMaxContentOffsetY比pinSectionHeaderVerticalOffset还小的情况
-                    //固定的位置就是contentInset.top
-                    adjustMainScrollViewToTargetContentInsetIfNeeded(inset: UIEdgeInsets(top: CGFloat(pinSectionHeaderVerticalOffset), left: 0, bottom: 0, right: 0))
-                }else if scrollView.contentOffset.y >= 0 {
-                    //因为设置了contentInset.top，所以顶部会有对应高度的空白区间，所以需要设置负数抵消掉
+                //因为设置了contentInset.top，所以顶部会有对应高度的空白区间，所以需要设置负数抵消掉
+                if scrollView.contentOffset.y >= 0 {
                     adjustMainScrollViewToTargetContentInsetIfNeeded(inset: UIEdgeInsets(top: -scrollView.contentOffset.y, left: 0, bottom: 0, right: 0))
-                }else if scrollView.contentOffset.y < 0 {
-                    adjustMainScrollViewToTargetContentInsetIfNeeded(inset: UIEdgeInsets.zero)
                 }
             }else if scrollView.contentOffset.y > CGFloat(pinSectionHeaderVerticalOffset) {
                 //固定的位置就是contentInset.top
