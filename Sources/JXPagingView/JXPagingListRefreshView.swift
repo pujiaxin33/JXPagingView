3723255b01d6f5a24cb5197ba2ae95a878b7c1b0
pujiaxin33 <317437084@qq.com>
2020-05-11 10:23:17 +0800

    swift版本优化pinSectionHeaderVerticalOffset相关逻辑

--- Sources/JXPagingView/JXPagingListRefreshView.swift
+++ Sources/JXPagingView/JXPagingListRefreshView.swift
@@ -19,7 +19,7 @@ open class JXPagingListRefreshView: JXPagingView {
 
     override open func preferredProcessMainTableViewDidScroll(_ scrollView: UIScrollView) {
         if pinSectionHeaderVerticalOffset != 0 {
-            if scrollView.contentOffset.y == 0 {
+            if scrollView.contentOffset.y <= 0 {
                 mainTableView.bounces = false
                 mainTableView.contentOffset = CGPoint.zero
                 return
