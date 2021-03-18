bd079e0cba46b636e6f9e376963fe6101b9f7a81
pujiaxin33 <317437084@qq.com>
2021-03-18 15:59:35 +0800

    优化initList方法，cell为nil时的处理

--- Sources/JXPagerView/JXPagerListContainerView.m
+++ Sources/JXPagerView/JXPagerListContainerView.m
@@ -431,16 +431,24 @@
     }
     _validListDict[@(index)] = list;
 
-    if (self.containerType == JXPagerListContainerType_ScrollView) {
-        [list listView].frame = CGRectMake(index*self.scrollView.bounds.size.width, 0, self.scrollView.bounds.size.width, self.scrollView.bounds.size.height);
-        [self.scrollView addSubview:[list listView]];
-    }else {
-        UICollectionViewCell *cell = [self.collectionView cellForItemAtIndexPath:[NSIndexPath indexPathForItem:index inSection:0]];
-        for (UIView *subview in cell.contentView.subviews) {
-            [subview removeFromSuperview];
+    switch (self.containerType) {
+        case JXPagerListContainerType_ScrollView: {
+            [list listView].frame = CGRectMake(index*self.scrollView.bounds.size.width, 0, self.scrollView.bounds.size.width, self.scrollView.bounds.size.height);
+            [self.scrollView addSubview:[list listView]];
+            break;
         }
-        [list listView].frame = cell.contentView.bounds;
-        [cell.contentView addSubview:[list listView]];
+        case JXPagerListContainerType_CollectionView: {
+            UICollectionViewCell *cell = [self.collectionView cellForItemAtIndexPath:[NSIndexPath indexPathForItem:index inSection:0]];
+            if (cell != nil) {
+                for (UIView *subview in cell.contentView.subviews) {
+                    [subview removeFromSuperview];
+                }
+                [list listView].frame = cell.contentView.bounds;
+                [cell.contentView addSubview:[list listView]];
+            }
+            break;
+        }
+
     }
 }
 
